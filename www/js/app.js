36149c7aed4a6769aa403ad93b2b523a821250cb
406798106@qq.com <406798106@qq.com>
2017-01-06 09:04:02 +0800

    ionic纯H5项目

---
+++ www/js/app.js
@@ -0,0 +1,40 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+  .config(function ($stateProvider, $urlRouterProvider) {
+    //产品列表
+    $stateProvider
+      .state('main', {//客服首页
+        url: '/main',
+        templateUrl: 'html/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('details', {//客服首页
+        url: '/details',
+        templateUrl: 'html/details.html',
+        controller: 'DetailsCtrl'
+      })
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('main');
+  })
---
+++ www/js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('starter.controllers', [])
+
+
+//客服页面
+  .controller('MainCtrl', function ($scope) {
+
+  })
+
+  //详情页面
+  .controller('DetailsCtrl', function ($scope) {
+
+  })
+
